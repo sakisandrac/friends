01a4054c2af0cb7f480853bfe1d9a4abe53e9883
“Saki <sakisandra.c@gmail.com>
2023-03-01 14:38:49 -0700

    Create an array called places

--- friends.js
+++ friends.js
@@ -2,3 +2,7 @@ var friends = ["Steve", "Ana", "Lucy", "Jason", "Chad"];
 console.log(friends[2]);
 console.log(friends[4]);
 console.log(friends[0]);
+
+var places = ["Italy", "Spain", "Iceland", "Sri Lanka", "North Dakota"];
+places.pop();
+console.log(places);
